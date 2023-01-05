105d059c77026cbf67861e28ac6ef94fe735e825
Kramer Brown <brownkramer63@gmail.com>
2023-01-05 16:56:50 -0600

    Initial commit

---
+++ src/main/java/com/cydeo/ApiReviewApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apireview/ApiReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
