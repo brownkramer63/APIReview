9ea7fe4968896a5e03c1728bf751a76ded21b53e
Kramer Brown <brownkramer63@gmail.com>
2023-02-28 17:20:08 -0600

    api review

--- src/main/java/com/cydeo/Entity/Jobs.java
+++ src/main/java/com/cydeo/Entity/Jobs.java
@@ -1,4 +1,23 @@
 package com.cydeo.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Jobs")
 public class Jobs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Jobtitle;
+    private String Company;
 }
