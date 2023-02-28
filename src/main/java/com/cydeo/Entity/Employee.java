8a731bbeb56dc0fb4246682b47441c2c22371861
Kramer Brown <brownkramer63@gmail.com>
2023-02-28 17:30:33 -0600

    api review

--- src/main/java/com/cydeo/Entity/Employee.java
+++ src/main/java/com/cydeo/Entity/Employee.java
@@ -1,5 +1,27 @@
 package com.cydeo.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Jobs(Job_title,company)VALUES('Java Develoer','Apple'),
+                                         ('C Developer','Google');
