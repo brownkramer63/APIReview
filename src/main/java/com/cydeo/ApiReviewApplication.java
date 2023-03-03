78e2fc9f0c1a7c6a48d9517f4af1d3b3a587c635
Kramer Brown <brownkramer63@gmail.com>
2023-03-03 17:18:53 -0600

    api review

--- src/main/java/com/cydeo/ApiReviewApplication.java
+++ src/main/java/com/cydeo/ApiReviewApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiReviewApplication {
@@ -9,5 +11,9 @@ public class ApiReviewApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiReviewApplication.class, args);
 	}
+	@Bean
+	public ModelMapper mapper(){
 
+		return new ModelMapper();
+	}
 }
